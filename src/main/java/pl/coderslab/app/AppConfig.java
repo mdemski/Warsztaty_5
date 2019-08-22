31d9e9895b2b5e089597f1ce49be06cddc45370a
Marcin Demski <m.demski@outlook.com>
2019-08-22 18:56:39 +0200

    Configurations files for project.

---
+++ src/main/java/pl/coderslab/app/AppConfig.java
@@ -0,0 +1,19 @@
+package pl.coderslab.app;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "pl.coderslab")
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureDefaultServletHandling(
+            DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
