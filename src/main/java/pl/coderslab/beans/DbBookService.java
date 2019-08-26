403f0b2f3eb96e7de342aeef99a283742b78537a
Marcin Demski <m.demski@outlook.com>
2019-08-26 20:52:24 +0200

    Database book service to put and get data about books to database and from database.

---
+++ src/main/java/pl/coderslab/beans/DbBookService.java
@@ -0,0 +1,38 @@
+package pl.coderslab.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.coderslab.dao.BookDao;
+
+import java.util.List;
+
+@Service
+public class DbBookService implements BookService {
+    @Autowired
+    BookDao bookDao;
+
+    @Override
+    public List<Book> getList() {
+        return bookDao.allBooks();
+    }
+
+    @Override
+    public Book getBook(long bookId) {
+        return bookDao.read(bookId);
+    }
+
+    @Override
+    public void addBook(Book book) {
+        bookDao.create(book);
+    }
+
+    @Override
+    public void deleteBook(long bookId) {
+        bookDao.delete(bookId);
+    }
+
+    @Override
+    public void editBook(Book book) {
+        bookDao.update(book);
+    }
+}
