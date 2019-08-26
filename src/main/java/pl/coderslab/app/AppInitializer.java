c3469c9c6c7f3b42cfd18f6071073f8a8b34a665
Marcin Demski <m.demski@outlook.com>
2019-08-26 20:50:02 +0200

    AppInitializer file.

--- src/main/java/pl/coderslab/app/AppInitializer.java
+++ src/main/java/pl/coderslab/app/AppInitializer.java
@@ -10,20 +10,25 @@ import javax.servlet.Filter;
 public class AppInitializer extends
         AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
-    protected Class<?>[] getRootConfigClasses() { return null; }
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[]{AppConfig.class};  }
+        return new Class[]{AppConfig.class};
+    }
 
     @Override
     protected String[] getServletMappings() {
-        return new String[]{"/"};   }
+        return new String[]{"/"};
+    }
+
     @Override
     protected Filter[] getServletFilters() {
         CharacterEncodingFilter characterEncodingFilter =
                 new CharacterEncodingFilter();
         characterEncodingFilter.setEncoding("UTF-8");
-        return new Filter[] { characterEncodingFilter };
+        return new Filter[]{characterEncodingFilter};
     }
 }
