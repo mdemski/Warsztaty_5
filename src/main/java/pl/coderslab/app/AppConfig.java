bc91a201cde5fd78442ce19996fedf4e4b6ea851
Marcin Demski <m.demski@outlook.com>
2019-08-26 20:49:27 +0200

    Added new configuration.

--- src/main/java/pl/coderslab/app/AppConfig.java
+++ src/main/java/pl/coderslab/app/AppConfig.java
@@ -1,10 +1,18 @@
 package pl.coderslab.app;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import pl.coderslab.beans.BookService;
+import pl.coderslab.beans.DbBookService;
+import pl.coderslab.beans.MemoryBookService;
+import pl.coderslab.dao.BookDao;
 
 @Configuration
 @EnableWebMvc
@@ -15,5 +23,27 @@ public class AppConfig extends WebMvcConfigurerAdapter {
     public void configureDefaultServletHandling(
             DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("http://localhost");
+    }
+
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver =
+                new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Bean
+    public BookDao bookDao(){
+        return new BookDao();
     }
 }
